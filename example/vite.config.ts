a7dd6b306ceb1eed8e46f91b22af00f4f7e75215
wenqing <wenqing@kerrylan.com>
2022-12-21 11:13:38 +0800

    docs: update example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -7,54 +7,7 @@ import { svg4VuePlugin } from 'vite-plugin-svg4vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    svg4VuePlugin({
-      svgoConfig: {
-        plugins: [
-          {
-            name: 'moveFillAttrToSvgNodeFromPath',
-            fn() {
-              return {
-                element: {
-                  enter: (node) => {
-                    if (node.name === 'svg') {
-                      if (!node.children || node.children.length <= 0) return
-
-                      const pathElements = node.children.filter(
-                        (v) => (v as any).name === 'path'
-                      )
-
-                      if (pathElements.length <= 0) return
-
-                      const allPathFillValue = pathElements.map(
-                        (v) => (v as any).attributes.fill
-                      )
-
-                      const hasPathFill = allPathFillValue.length > 0
-
-                      const isMonochromeSvg =
-                        hasPathFill &&
-                        [...new Set(allPathFillValue)].length === 1
-
-                      if (isMonochromeSvg) {
-                        node.attributes.fill = allPathFillValue[0]
-
-                        pathElements.forEach((v) => {
-                          delete (v as any).attributes.fill
-                        })
-                      }
-                    }
-                  },
-                },
-              }
-            },
-          },
-        ],
-      },
-    }),
-    vueJsx(),
-  ],
+  plugins: [vue(), svg4VuePlugin(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
