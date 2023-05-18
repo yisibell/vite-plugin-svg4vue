93ecab614041afdd50f7bc709ad4cdb254ca69a0
wenqing <wenqing@kerrylan.com>
2023-05-18 16:26:39 +0800

    fix(example): example 404 error

--- example/src/router/index.ts
+++ example/src/router/index.ts
@@ -1,16 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/home',
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: () => import('@/views/Home.vue'),
+      name: 'home',
+      component: Home,
     },
   ],
 })
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       svgoConfig: {
         plugins: [],
       },
+      enableBuildCache: false,
     }),
     vueJsx(),
   ],
