3d261d219886b7241261cf1bfc5b099c14f76197
elenh <yisiwings@163.com>
2025-04-03 00:27:48 +0800

    fix: class namespaces

--- example/src/App.vue
+++ example/src/App.vue
@@ -16,12 +16,21 @@
         { href: LINKS.repo, title: 'Github' },
       ]"
     />
+    <span v-html="Icon"></span>
+    <span v-html="Icon2"></span>
+
+    <Icon3 font-size="60px" />
+    <Icon4 font-size="60px" />
     <RouterView />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
+import Icon from '@/icons/filter/strapssleeves/hover/long_sleeve.svg?raw'
+import Icon4 from '@/icons/filter/strapssleeves/hover/long_sleeve.svg?component'
+import Icon2 from '@/icons/filter/strapssleeves/selected/long_sleeves.svg?raw'
+import Icon3 from '@/icons/filter/strapssleeves/selected/long_sleeves.svg?component'
 
 import { LINKS } from '@/settings'
 </script>
--- src/optimizeSvg.ts
+++ src/optimizeSvg.ts
@@ -1,5 +1,10 @@
 import * as SVGO from 'svgo'
 import type { Config as SvgoConfig } from 'svgo'
+import { v4 as uuidv4 } from 'uuid'
+
+const genUuid = () => {
+  return `a-${uuidv4().slice(0, 5)}`
+}
 
 /**
  * Optimize svg with svgo
@@ -9,6 +14,13 @@ async function optimizeSvg(
   path: string,
   finalSvgoConfig: SvgoConfig = {},
 ) {
+  finalSvgoConfig.plugins?.push({
+    name: 'prefixIds',
+    params: {
+      prefix: () => genUuid(),
+    },
+  })
+
   const { data } = await SVGO.optimize(content, {
     ...finalSvgoConfig,
     path,
